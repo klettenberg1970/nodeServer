68a8b8006fb918019b5a417296920bd99e302d9f
klettenberg1970 <bastenstefan@gmail.com>
2026-06-02 15:13:16 +0200

    4.Update 02.06.2026

--- src/controllers/RSS/rssController.js
+++ src/controllers/RSS/rssController.js
@@ -5,7 +5,7 @@ const parser = new Parser();
 
 
 export async function getnamen(req, res) {
-  
+
   const feeds = await RSS.find();
   const sortierteNamen = feeds
     .map(element => element.name)
@@ -14,10 +14,10 @@ export async function getnamen(req, res) {
   res.json(sortierteNamen || []);
 }
 
-export async function getAllFeeds (req,res){
+export async function getAllFeeds(req, res) {
   const feeds = await RSS.find();
   console.log(feeds)
-  res.json({feeds:feeds})
+  res.json({ feeds: feeds })
 }
 
 export async function getfeeds(req, res) {
@@ -36,19 +36,19 @@ export async function getfeeds(req, res) {
 }
 
 export async function createFeeds(req, res) {
-    try {
-        const { kategorie, name, url } = req.body;
-        await RSS.create({ kategorie, name, url });
-        console.log(`Name: ${name} wurde erstellt`);
-        res.status(201).json({ success: true });
-    } catch (err) {
-        res.status(500).json({ error: 'Serverfehler' });
-    }
+  try {
+    const { kategorie, name, url } = req.body;
+    await RSS.create({ kategorie, name, url });
+    console.log(`Name: ${name} wurde erstellt`);
+    res.status(201).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: 'Serverfehler' });
+  }
 }
 
 
-export async function deleterss (req,res) {
+export async function deleterss(req, res) {
   const rssname = req.body.name;
-     const geloescht = await RSS.findOneAndDelete({ name: rssname });
- res.status(200).json({message: `${rssname} wurde gelöscht` });
+  const geloescht = await RSS.findOneAndDelete({ name: rssname });
+  res.status(200).json({ message: `${rssname} wurde gelöscht` });
 }
