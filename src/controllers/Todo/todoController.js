4ec2e68eb19d26caee5dc5e8983df34188e599f1
klettenberg1970 <bastenstefan@gmail.com>
2026-06-14 22:24:05 +0200

    16. Update 14.06. 2026

--- src/controllers/Todo/todoController.js
+++ src/controllers/Todo/todoController.js
@@ -1,23 +1,21 @@
 import asyncHandler from '../../middleware/asyncHandler.js';
 import {dateiLesen, dateiSchreiben} from './toDoDoc.js';
 
-// NICHT hier oben auslesen!
-// const toDoId = process.env.TODOID;  // ← Das ist das Problem!
+const TODOID = '1LotmSZ37UUnLDGcOXpKEGTwoBtJDk0C1wIqB1VayEVc'
 
 export const getTodoDatei = asyncHandler(async (req,res) =>{
-     const toDoId = process.env.TODOID;  // ← Hier zur Laufzeit lesen!
+     const toDoId = process.env.TODOID || TODOID;  
      
      if (!toDoId) {
          return res.status(500).json({ error: 'TODOID ist nicht definiert' });
      }
      
      const inhalt = await dateiLesen(toDoId);
-     
      res.json({ inhalt });
 })
 
 export const changeTodoDatei = asyncHandler(async (req,res) =>{
-     const toDoId = process.env.TODOID;  // ← Auch hier zur Laufzeit!
+     const toDoId = process.env.TODOID || TODOID;  
      const text = req.body.text;
      
      if (!toDoId) {
