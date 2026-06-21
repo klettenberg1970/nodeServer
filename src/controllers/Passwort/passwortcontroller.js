6d8182154060f60efa4483764b5d75456d47c543
klettenberg1970 <bastenstefan@gmail.com>
2026-06-21 16:01:14 +0200

    Update 21.06.2026

--- src/controllers/Passwort/passwortcontroller.js
+++ src/controllers/Passwort/passwortcontroller.js
@@ -1,23 +1,17 @@
 import 'dotenv/config'; 
-import jwt from 'jsonwebtoken';
+
 
 export const checkPasswort = (req, res) => {
     const { password } = req.body; // Destructuring ist sauberer
+  
     const correctPassword = process.env.PORTFOLIO_PASSWORD;
-    const JWT_SECRET = process.env.JWT_SECRET;
-    
-
+ 
     if (password === correctPassword) {
-        // Token erstellen
-        const token = jwt.sign(
-            { user: 'admin' }, 
-            JWT_SECRET, 
-            { expiresIn: '5m' }
-        );
+       
 
         // Token an das Frontend senden
-        return res.json({ message: true, token: token });
+        return res.json({ message: true});
     } else {
-        return res.json({ message: false, message: 'Falsches Passwort' });
+        return res.json({ message: false });
     }
 };
