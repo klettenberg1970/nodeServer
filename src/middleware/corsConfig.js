e3d07a32d40a56f74c5305252da4bb7daad5dd24
klettenberg1970 <bastenstefan@gmail.com>
2026-08-26 12:10:02 +0200

    2.Update 26.08.2026

--- src/middleware/corsConfig.js
+++ src/middleware/corsConfig.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     // ... deine anderen Einträge
     'http://localhost:3000',
     'http://localhost:5173',
+    'http://127.0.0.1:5173', 
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:8080',
